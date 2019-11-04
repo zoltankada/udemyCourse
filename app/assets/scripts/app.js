3f3cc04dff1f8397ea34425da0bf50df27e4c750
zoltan.kada <zoltan.kada@edreamsodigeo.com>
2019-11-04 16:14:19 +0100

    asdsadsdsadsa

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,10 @@ import $ from 'jquery';
 import MobileMenu from "./modules/mobileMenu";
 import RevealOnScroll from "./modules/revealOnScroll";
 import StickyHeader from "./modules/stickyHeader";
+import Modal from "./modules/modal";
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
