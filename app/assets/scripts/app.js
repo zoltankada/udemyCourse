5b83de2260710603e9623ce1a5cfc704d724f9cb
zoltan.kada <zoltan.kada@edreamsodigeo.com>
2019-11-04 14:17:05 +0100

    sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from "./modules/mobileMenu";
 import RevealOnScroll from "./modules/revealOnScroll";
+import StickyHeader from "./modules/stickyHeader";
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
